50affff2b17b3b705e99aaeff3b91016c3416a51
subquatic-pierre <subquatic.pierre@gmail.com>
2020-11-27 14:29:42 +0400

    Added AED as default currency to start script

--- scripts/start-api.sh
+++ scripts/start-api.sh
@@ -7,6 +7,8 @@ export 'MEDIA_URL=/media/'
 export 'SECRET_KEY=supersecretkey'
 export 'DEBUG=True'
 export 'VATLAYER_ACCESS_KEY=cedf249c2eed18bb4f390a3665bd9f4a'
+export 'DEFAULT_COUNTRY=AE'
+export 'DEFAULT_CURRENCY=AED'
 
 source venv/bin/activate
 cd api
@@ -14,6 +16,6 @@ cd api
 # python3 manage.py migrate 
 # python3 manage.py populatedb --createsuperuser
 # python3 manage.py createsuperuser
-#python3 manage.py shell 
+# python3 manage.py shell 
 python3 manage.py runserver 0.0.0.0:8000
 
